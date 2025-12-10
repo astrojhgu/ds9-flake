a814f88dffddd0ed8a0146c65f876281490c4373
GU Junhua <jhgu@nao.cas.cn>
2025-12-10 13:35:58 +0800

    modified:   ds9 	modified:   flake.lock 	modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "DS9 binary flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
